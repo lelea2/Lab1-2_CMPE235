8756ac3253de9ca80ac02356b77757a41638fee0
Khanh Dao <kdao@Khanhs-MBP.attlocal.net>
2016-03-01 05:00:01 -0800

    Initial check in for sharing

--- app/src/main/java/com/example/kdao/lab1_cmpe235/SharingActivity.java
+++ app/src/main/java/com/example/kdao/lab1_cmpe235/SharingActivity.java
@@ -1,7 +1,10 @@
 package com.example.kdao.lab1_cmpe235;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SharingActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class SharingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sharing);
+        //Share button click listener.
+        Button btn_share=(Button) findViewById(R.id.share_button);
+        btn_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareIt();
+            }
+        });
     }
+    private void shareIt() {
+        //sharing button implementation
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "AndroidSolved");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Smart tree in smart city, we got smart life ");
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+    }
+
 }
