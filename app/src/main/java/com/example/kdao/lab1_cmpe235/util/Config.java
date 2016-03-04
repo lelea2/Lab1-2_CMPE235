24c77e7d59fba6b32e555ca0c1225ce8de2a24e2
Khanh Dao <kdao@Khanhs-MBP.attlocal.net>
2016-03-04 03:16:12 -0800

    Initial check in for android app

---
+++ app/src/main/java/com/example/kdao/lab1_cmpe235/util/Config.java
@@ -0,0 +1,7 @@
+package com.example.kdao.lab1_cmpe235.util;
+
+//Maintain constant in Java
+public class Config {
+    public static final String GOOGLE_API = "AIzaSyBOV7pJDxO_vifta4XzE4b_RnDTs0NeV0o";
+    public static final int RADIUS = 20000;
+}
