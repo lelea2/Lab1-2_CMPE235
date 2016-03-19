fd821558bde7da6c7a03a5c1c411057ad0dfffd8
Khanh Dao <kdao@Khanhs-MBP.attlocal.net>
2016-03-19 16:42:26 -0700

    Signin successfully

--- app/src/main/java/com/example/kdao/lab1_cmpe235/SigninActivity.java
+++ app/src/main/java/com/example/kdao/lab1_cmpe235/SigninActivity.java
@@ -1,11 +1,18 @@
 package com.example.kdao.lab1_cmpe235;
 
+import android.net.Uri;
+import android.preference.Preference;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.os.AsyncTask;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
@@ -15,11 +22,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//import cz.msebera.android.httpclient.*;
-//import com.loopj.android.http.*;
+import org.apache.http.*;
+import org.apache.http.entity.*;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.*;
+
+import java.io.*;
 
 import com.example.kdao.lab1_cmpe235.util.Utility;
 import com.example.kdao.lab1_cmpe235.util.Config;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class SigninActivity extends AppCompatActivity {
     ProgressDialog progressDialog;
@@ -38,9 +52,9 @@ public class SigninActivity extends AppCompatActivity {
      * Private method to get elemnt on page
      */
     private void getElem() {
-        errMsg = (TextView)findViewById(R.id.login_error);
-        emailText = (EditText)findViewById(R.id.loginEmail);
-        pwdText = (EditText)findViewById(R.id.loginPassword);
+        errMsg = (TextView) findViewById(R.id.login_error);
+        emailText = (EditText) findViewById(R.id.loginEmail);
+        pwdText = (EditText) findViewById(R.id.loginPassword);
         // Instantiate Progress Dialog object
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Please wait...");
@@ -49,26 +63,91 @@ public class SigninActivity extends AppCompatActivity {
 
     /**
      * Method trigger when click on login user
+     *
      * @method loginUser
      */
     public void loginUser(View view) {
         String email = emailText.getText().toString();
         String password = pwdText.getText().toString();
-        // Instantiate Http Request Param Object
-        //RequestParams params = new RequestParams();
-        // When Email Edit View and Password Edit View have values other than Null
-        if(!Utility.isEmptyString(email) && !Utility.isEmptyString(password)) {
+        if (!Utility.isEmptyString(email) && !Utility.isEmptyString(password)) {
             // When Email entered is Valid
-            if(Utility.isEmailValid(email)){
-               // params.put("username", email);
-               // params.put("password", password);
-                //invokeWS(params);
+            if (Utility.isEmailValid(email)) {
+                logUserIn(email, password);
             } else { // When Email is invalid
                 Toast.makeText(getApplicationContext(), "Please enter valid email", Toast.LENGTH_LONG).show();
             }
         } else { //Empty form handle
             Toast.makeText(getApplicationContext(), "Please fill in the form", Toast.LENGTH_LONG).show();
         }
+    }
+
+    /**
+     * Private function for user login
+     *
+     * @method logUserIn
+     */
+    private void logUserIn(String email, String password) {
+        class SendPostReqAsyncTask extends AsyncTask<String, Void, String> {
+            @Override
+            protected String doInBackground(String... params) {
+                String userEmail = params[0];
+                String userPassword = params[1];
+                HttpClient httpClient = new DefaultHttpClient();
+                System.out.println("String url=" + Config.BASE_URL + "/user/login");
+                HttpPost httpPost = new HttpPost(Config.BASE_URL + "/user/login");
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("email", userEmail);
+                    json.put("password", userPassword);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    StringEntity se = new StringEntity(json.toString());
+                    System.out.println(se);
+                    se.setContentEncoding("UTF-8");
+                    httpPost.setEntity(se);
+                    try {
+                        HttpResponse httpResponse = httpClient.execute(httpPost);
+                        InputStream inputStream = httpResponse.getEntity().getContent();
+                        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                        StringBuilder stringBuilder = new StringBuilder();
+                        String bufferedStrChunk = null;
+                        while((bufferedStrChunk = bufferedReader.readLine()) != null) {
+                            stringBuilder.append(bufferedStrChunk);
+                        }
+                        return stringBuilder.toString();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception uee) {
+                    System.out.println("An Exception given because of UrlEncodedFormEntity argument :" + uee);
+                    uee.printStackTrace();
+                }
+                return null;
+            }
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                //System.out.println(result);
+                String userId = "";
+                try {
+                    JSONObject jObject  = new JSONObject(result);
+                    userId = (String) jObject.get("userId");
+                } catch(Exception ex) {
+                }
+                if(!Utility.isEmptyString(userId)){
+                    //Toast.makeText(getApplicationContext(), "working", Toast.LENGTH_LONG).show();
+                    navigateToMainActivity();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Invalid email or password. Please " +
+                            "try again!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
+        sendPostReqAsyncTask.execute(email, password);
     }
 
     /**
@@ -82,50 +161,12 @@ public class SigninActivity extends AppCompatActivity {
     }
 
     /**
-     * Private method handle
+     * Private function to navigate to home activity
+     * @method navigateToMainActivity
      */
-    /*public void invokeWS(RequestParams params){
-        progressDialog.show();
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.post(Config.BASE_URL + "/user", params, new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
-                progressDialog.hide();
-                try {
-                    /*JSONObject obj = new JSONObject(response);
-                    if (obj.getBoolean("status")) {
-                        Toast.makeText(getApplicationContext(), "You are successfully logged in!", Toast.LENGTH_LONG).show();
-                        // Navigate to Home screen
-                        //navigatetoHomeActivity();
-                    }
-                    // Else display error message
-                    else {
-                        errMsg.setText(obj.getString("error_msg"));
-                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
-                    }*/
-               /* } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
-                    e.printStackTrace();
-
-                }
-            }
-            // When the response returned by REST has Http response code other than '200'
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse,
-                                  Throwable e) {
-                // Hide Progress Dialog
-                progressDialog.hide();
-                // When Http response code is '404'
-                if (statusCode == 404) {
-                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
-                } else if (statusCode == 500) { // When Http response code is '500'
-                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
-                } else { // When Http response code other than 404, 500
-                    Toast.makeText(getApplicationContext(), "Unexpected Error occcur.", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }*/
-
+    private void navigateToMainActivity() {
+        Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(mainIntent);
+    }
 }
